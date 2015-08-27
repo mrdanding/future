997e6d544979bb960a132a73f7b495123ebd1795
mrdanding <xiashuaiqi@gmail.com>
2015-08-27 22:14:33 +0800

    finish database design 1.0

---
+++ src/entity/TaskEntity.java
@@ -0,0 +1,203 @@
+package entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * @author shuaiqi.xsq, 15/8/27
+ */
+@Entity
+@Table(name = "task", schema = "", catalog = "Future")
+public class TaskEntity {
+    private String taskId;
+    private String sourceUserName;
+    private String executorUserName;
+    private String taskName;
+    private Timestamp taskPublishTime;
+    private Timestamp taskDispatchTime;
+    private Integer taskTotalCount;
+    private Integer taskFinishCount;
+    private String keyword;
+    private String taskUrl;
+    private String shipSource;
+    private String priceMin;
+    private String priceMax;
+
+    @Id
+    @Column(name = "taskId")
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    @Basic
+    @Column(name = "sourceUserName")
+    public String getSourceUserName() {
+        return sourceUserName;
+    }
+
+    public void setSourceUserName(String sourceUserName) {
+        this.sourceUserName = sourceUserName;
+    }
+
+    @Basic
+    @Column(name = "executorUserName")
+    public String getExecutorUserName() {
+        return executorUserName;
+    }
+
+    public void setExecutorUserName(String executorUserName) {
+        this.executorUserName = executorUserName;
+    }
+
+    @Basic
+    @Column(name = "taskName")
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Basic
+    @Column(name = "taskPublishTime")
+    public Timestamp getTaskPublishTime() {
+        return taskPublishTime;
+    }
+
+    public void setTaskPublishTime(Timestamp taskPublishTime) {
+        this.taskPublishTime = taskPublishTime;
+    }
+
+    @Basic
+    @Column(name = "taskDispatchTime")
+    public Timestamp getTaskDispatchTime() {
+        return taskDispatchTime;
+    }
+
+    public void setTaskDispatchTime(Timestamp taskDispatchTime) {
+        this.taskDispatchTime = taskDispatchTime;
+    }
+
+    @Basic
+    @Column(name = "taskTotalCount")
+    public Integer getTaskTotalCount() {
+        return taskTotalCount;
+    }
+
+    public void setTaskTotalCount(Integer taskTotalCount) {
+        this.taskTotalCount = taskTotalCount;
+    }
+
+    @Basic
+    @Column(name = "taskFinishCount")
+    public Integer getTaskFinishCount() {
+        return taskFinishCount;
+    }
+
+    public void setTaskFinishCount(Integer taskFinishCount) {
+        this.taskFinishCount = taskFinishCount;
+    }
+
+    @Basic
+    @Column(name = "keyword")
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Basic
+    @Column(name = "taskUrl")
+    public String getTaskUrl() {
+        return taskUrl;
+    }
+
+    public void setTaskUrl(String taskUrl) {
+        this.taskUrl = taskUrl;
+    }
+
+    @Basic
+    @Column(name = "shipSource")
+    public String getShipSource() {
+        return shipSource;
+    }
+
+    public void setShipSource(String shipSource) {
+        this.shipSource = shipSource;
+    }
+
+    @Basic
+    @Column(name = "priceMin")
+    public String getPriceMin() {
+        return priceMin;
+    }
+
+    public void setPriceMin(String priceMin) {
+        this.priceMin = priceMin;
+    }
+
+    @Basic
+    @Column(name = "priceMax")
+    public String getPriceMax() {
+        return priceMax;
+    }
+
+    public void setPriceMax(String priceMax) {
+        this.priceMax = priceMax;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TaskEntity that = (TaskEntity) o;
+
+        if (taskId != null ? !taskId.equals(that.taskId) : that.taskId != null) return false;
+        if (sourceUserName != null ? !sourceUserName.equals(that.sourceUserName) : that.sourceUserName != null)
+            return false;
+        if (executorUserName != null ? !executorUserName.equals(that.executorUserName) : that.executorUserName != null)
+            return false;
+        if (taskName != null ? !taskName.equals(that.taskName) : that.taskName != null) return false;
+        if (taskPublishTime != null ? !taskPublishTime.equals(that.taskPublishTime) : that.taskPublishTime != null)
+            return false;
+        if (taskDispatchTime != null ? !taskDispatchTime.equals(that.taskDispatchTime) : that.taskDispatchTime != null)
+            return false;
+        if (taskTotalCount != null ? !taskTotalCount.equals(that.taskTotalCount) : that.taskTotalCount != null)
+            return false;
+        if (taskFinishCount != null ? !taskFinishCount.equals(that.taskFinishCount) : that.taskFinishCount != null)
+            return false;
+        if (keyword != null ? !keyword.equals(that.keyword) : that.keyword != null) return false;
+        if (taskUrl != null ? !taskUrl.equals(that.taskUrl) : that.taskUrl != null) return false;
+        if (shipSource != null ? !shipSource.equals(that.shipSource) : that.shipSource != null) return false;
+        if (priceMin != null ? !priceMin.equals(that.priceMin) : that.priceMin != null) return false;
+        if (priceMax != null ? !priceMax.equals(that.priceMax) : that.priceMax != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = taskId != null ? taskId.hashCode() : 0;
+        result = 31 * result + (sourceUserName != null ? sourceUserName.hashCode() : 0);
+        result = 31 * result + (executorUserName != null ? executorUserName.hashCode() : 0);
+        result = 31 * result + (taskName != null ? taskName.hashCode() : 0);
+        result = 31 * result + (taskPublishTime != null ? taskPublishTime.hashCode() : 0);
+        result = 31 * result + (taskDispatchTime != null ? taskDispatchTime.hashCode() : 0);
+        result = 31 * result + (taskTotalCount != null ? taskTotalCount.hashCode() : 0);
+        result = 31 * result + (taskFinishCount != null ? taskFinishCount.hashCode() : 0);
+        result = 31 * result + (keyword != null ? keyword.hashCode() : 0);
+        result = 31 * result + (taskUrl != null ? taskUrl.hashCode() : 0);
+        result = 31 * result + (shipSource != null ? shipSource.hashCode() : 0);
+        result = 31 * result + (priceMin != null ? priceMin.hashCode() : 0);
+        result = 31 * result + (priceMax != null ? priceMax.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/entity/UserEntity.java
@@ -0,0 +1,150 @@
+package entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * @author shuaiqi.xsq, 15/8/27
+ */
+@Entity
+@Table(name = "user", schema = "", catalog = "Future")
+public class UserEntity {
+    private String userName;
+    private String mobileNumber;
+    private String email;
+    private Integer currentCredit;
+    private Integer totalBoughtCredit;
+    private Integer totalConsumedCredit;
+    private Timestamp registerTime;
+    private Integer todayWorkload;
+    private Integer yesterdayWorkload;
+
+    @Id
+    @Column(name = "userName")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Basic
+    @Column(name = "mobileNumber")
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "currentCredit")
+    public Integer getCurrentCredit() {
+        return currentCredit;
+    }
+
+    public void setCurrentCredit(Integer currentCredit) {
+        this.currentCredit = currentCredit;
+    }
+
+    @Basic
+    @Column(name = "totalBoughtCredit")
+    public Integer getTotalBoughtCredit() {
+        return totalBoughtCredit;
+    }
+
+    public void setTotalBoughtCredit(Integer totalBoughtCredit) {
+        this.totalBoughtCredit = totalBoughtCredit;
+    }
+
+    @Basic
+    @Column(name = "totalConsumedCredit")
+    public Integer getTotalConsumedCredit() {
+        return totalConsumedCredit;
+    }
+
+    public void setTotalConsumedCredit(Integer totalConsumedCredit) {
+        this.totalConsumedCredit = totalConsumedCredit;
+    }
+
+    @Basic
+    @Column(name = "registerTime")
+    public Timestamp getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(Timestamp registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    @Basic
+    @Column(name = "todayWorkload")
+    public Integer getTodayWorkload() {
+        return todayWorkload;
+    }
+
+    public void setTodayWorkload(Integer todayWorkload) {
+        this.todayWorkload = todayWorkload;
+    }
+
+    @Basic
+    @Column(name = "yesterdayWorkload")
+    public Integer getYesterdayWorkload() {
+        return yesterdayWorkload;
+    }
+
+    public void setYesterdayWorkload(Integer yesterdayWorkload) {
+        this.yesterdayWorkload = yesterdayWorkload;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
+        if (mobileNumber != null ? !mobileNumber.equals(that.mobileNumber) : that.mobileNumber != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (currentCredit != null ? !currentCredit.equals(that.currentCredit) : that.currentCredit != null)
+            return false;
+        if (totalBoughtCredit != null ? !totalBoughtCredit.equals(that.totalBoughtCredit) : that.totalBoughtCredit != null)
+            return false;
+        if (totalConsumedCredit != null ? !totalConsumedCredit.equals(that.totalConsumedCredit) : that.totalConsumedCredit != null)
+            return false;
+        if (registerTime != null ? !registerTime.equals(that.registerTime) : that.registerTime != null) return false;
+        if (todayWorkload != null ? !todayWorkload.equals(that.todayWorkload) : that.todayWorkload != null)
+            return false;
+        if (yesterdayWorkload != null ? !yesterdayWorkload.equals(that.yesterdayWorkload) : that.yesterdayWorkload != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userName != null ? userName.hashCode() : 0;
+        result = 31 * result + (mobileNumber != null ? mobileNumber.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (currentCredit != null ? currentCredit.hashCode() : 0);
+        result = 31 * result + (totalBoughtCredit != null ? totalBoughtCredit.hashCode() : 0);
+        result = 31 * result + (totalConsumedCredit != null ? totalConsumedCredit.hashCode() : 0);
+        result = 31 * result + (registerTime != null ? registerTime.hashCode() : 0);
+        result = 31 * result + (todayWorkload != null ? todayWorkload.hashCode() : 0);
+        result = 31 * result + (yesterdayWorkload != null ? yesterdayWorkload.hashCode() : 0);
+        return result;
+    }
+}
